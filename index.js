1a080dc1c7c6a84f44e9c7cfcfa5ab0c4b7837e1
Michaela Olson <mco44@cornell.edu>
2018-05-12 17:56:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file
-function findMatching(drivers, key) {
-  return drivers; 
+function findMatching(drivers, match) {
+  let matchyBois = drivers.filter(match)
+  return matchyBois;
 }
