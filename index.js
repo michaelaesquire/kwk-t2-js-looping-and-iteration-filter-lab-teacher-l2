f786032485f1c9b36ea6dbfc2181d1ca758f7048
Michaela Olson <mco44@cornell.edu>
2018-05-12 18:37:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file
+
+/*function inside(drivers,match) {
+  return drivers === match;
+}*/
+
+
+function findMatching(drivers, match) {
+  let matchyBois = drivers.filter(function(matches) {
+    let doubleCheck = match.toLowerCase();
+    if (matches === match) {
+        return matches === match;
+    }
+    else if (matches === doubleCheck) {
+        return matches === doubleCheck;
+    }
+  } );
+  return matchyBois;
+}
+
+function fuzzyMatch(drivers, match) {
+    return drivers.filter(function(el) {
+      return el.toLowerCase().indexOf(match.toLowerCase()) == 0;
+    })
+
+}
+
+function matchName(drivers,match) {
+  let matchyBois = drivers.filter(function(matches) {
+
+      return matches.name === match;
+
+
+  } );
+  return matchyBois;
+}
