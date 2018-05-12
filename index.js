61e0815becca611e541f3c7354c12729270beee7
Michaela Olson <mco44@cornell.edu>
2018-05-12 18:00:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function inside(drivers,match) {
   return drivers;
 }
 
-  
+
 function findMatching(drivers, match) {
-  let matchyBois = drivers.filter(function );
+  let matchyBois = drivers.filter(inside(drivers,match) );
   return matchyBois;
 }
